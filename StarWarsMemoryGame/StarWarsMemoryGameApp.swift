34509f553e8d4837c52fe935f4dd754940f8ae56
ribotinho <ribotinho@gmail.com>
2022-09-30 09:42:10 +0200

    Initial Commit

---
+++ StarWarsMemoryGame/StarWarsMemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsMemoryGameApp.swift
+//  StarWarsMemoryGame
+//
+//  Created by Pau Ribot Pujolras on 30/9/22.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsMemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
